d623950547d09c4b06bde10aaf564e2a1cfbf317
Rustam-RuKo <rustam1ruz@gmail.com>
2025-07-01 22:06:05 +0800

    feat authorization: implemented base authorization logic along with relevant unit tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module kallisto-backend
+
+go 1.20
---
+++ services/admin/internal/usecases/auth_usecase.go
@@ -0,0 +1,8 @@
+package usecases
+
+import "services/admin/internal/models"
+
+type AuthUseCase interface {
+	Register(req models.RegisterRequest) error
+	Login(req models.LoginRequest) (string, error)
+}
